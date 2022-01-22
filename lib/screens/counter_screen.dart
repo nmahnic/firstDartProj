082817487aeaeb8a884b2ee7c6191ff669c8fa79
Nicolas Mahnic <nico.mahnic94@gmail.com>
2022-01-21 21:45:22 -0300

    Perform and send functions by referense widget to another

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -12,6 +12,10 @@ class CounterScren extends StatefulWidget {
 class _CounterScrenState extends State<CounterScren> {
   int _counter = 0;
 
+  void increase () => {setState( () => {_counter++})};
+  void decrease () => {setState( () => {_counter--})};
+  void reset () => {setState( () => {_counter=0})};
+
   @override
   Widget build(BuildContext context) {
 
@@ -32,40 +36,58 @@ class _CounterScrenState extends State<CounterScren> {
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            FloatingActionButton(
-              backgroundColor: Colors.blue,
-              foregroundColor: Colors.black,
-              onPressed: () {
-                _counter++;
-                setState(() {});
-              },
-              child: const Icon(Icons.exposure_plus_1),
-            ),
-            const SizedBox(width: 20),
-            FloatingActionButton.extended(
-              backgroundColor: Colors.blue,
-              foregroundColor: Colors.black,
-              onPressed: () {
-                _counter = 0;
-                setState(() {});
-              },
-              label: const Text('RESET')
-            ),
-            const SizedBox(width: 20),
-            FloatingActionButton(
-              backgroundColor: Colors.blue,
-              foregroundColor: Colors.black,
-              onPressed: () {
-                _counter--;
-                setState(() {});
-              },
-              child: const Icon(Icons.exposure_minus_1),
-            ),
-          ],
-        ),
+      floatingActionButton: CustomFloatingActions(
+        increseFun: increase,
+        decreaseFun: decrease,
+        resetFun: reset,
+      ),
     );
+  }
+}
+
+class CustomFloatingActions extends StatelessWidget {
+
+  final Function increseFun;
+  final Function decreaseFun;
+  final Function resetFun;
+
+  const CustomFloatingActions({
+    Key? key, 
+    required this.increseFun,
+    required this.decreaseFun,
+    required this.resetFun,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FloatingActionButton(
+            backgroundColor: Colors.blue,
+            foregroundColor: Colors.black,
+            onPressed: () => increseFun(),
+            child: const Icon(Icons.exposure_plus_1),
+          ),
+          
+          const SizedBox(width: 20),
+
+          FloatingActionButton.extended(
+            backgroundColor: Colors.blue,
+            foregroundColor: Colors.black,
+            onPressed: () => resetFun(),
+            label: const Text('RESET')
+          ),
+
+          const SizedBox(width: 20),
+
+          FloatingActionButton(
+            backgroundColor: Colors.blue,
+            foregroundColor: Colors.black,
+            onPressed: () => decreaseFun(),
+            child: const Icon(Icons.exposure_minus_1),
+          ),
+        ],
+      );
   }
 }
