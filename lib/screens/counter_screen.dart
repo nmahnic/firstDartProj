33fe46e426676a7fe33dbc8708682115e640ca82
Nicolas Mahnic <nico.mahnic94@gmail.com>
2022-01-21 21:20:26 -0300

    plus, minus, reset

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -32,25 +32,39 @@ class _CounterScrenState extends State<CounterScren> {
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton:
-        FloatingActionButton(
-          backgroundColor: Colors.blue,
-          foregroundColor: Colors.black,
-          onPressed: () {
-            print(_counter);
-            /*
-            _counter++;
-            setState(() {});
-            */
-            /*
-            _counter++;
-            setState(() => {});
-            */
-            setState(() => { //callback
-              _counter++
-            });
-          },
-          child: const Icon(Icons.add),
+      floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FloatingActionButton(
+              backgroundColor: Colors.blue,
+              foregroundColor: Colors.black,
+              onPressed: () {
+                _counter++;
+                setState(() {});
+              },
+              child: const Icon(Icons.exposure_plus_1),
+            ),
+            const SizedBox(width: 20),
+            FloatingActionButton.extended(
+              backgroundColor: Colors.blue,
+              foregroundColor: Colors.black,
+              onPressed: () {
+                _counter = 0;
+                setState(() {});
+              },
+              label: const Text('RESET')
+            ),
+            const SizedBox(width: 20),
+            FloatingActionButton(
+              backgroundColor: Colors.blue,
+              foregroundColor: Colors.black,
+              onPressed: () {
+                _counter--;
+                setState(() {});
+              },
+              child: const Icon(Icons.exposure_minus_1),
+            ),
+          ],
         ),
     );
   }
